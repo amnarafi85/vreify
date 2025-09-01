8b3c4fe4ac8a0d63a509ab0daee49f7dc0a81e27
Amna <amna123@gmail.com>
2025-09-02 04:46:36 +0500

    Fix vite config and add redirec12345qwtRs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true, // optional
-  },
-  build: {
-    outDir: "dist",
-  },
+  base: "/certificate-verify/", // repo name here
 });
